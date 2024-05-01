839c92a91217e9baa113489b0690e6dd566cca45
菜鸟 <wdhlzd@163.com>
2024-05-01 11:03:46 +0800

    :technologist: prettier、eslint、vscode settings 配置优化
    Signed-off-by: 菜鸟 <wdhlzd@163.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,3 @@
-import config from '@antfu/eslint-config';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default config(
-  {
-    ignores: ['**/cache'],
-  },
-  {
-    files: ['packages/plugin/src/types.ts'],
-    rules: {
-      'ts/no-unused-vars': ['error', { varsIgnorePattern: '^S$|^Store$' }],
-      'unused-imports/no-unused-vars': ['error', { varsIgnorePattern: '^S$|^Store$' }],
-    },
-  },
-  eslintConfigPrettier,
-);
+export default [eslintPluginPrettierRecommended];
